9ce379662ab012b5057a0eb7cca93131d563ea09
Tara Kamshad <c24tk@dalton.org>
2022-12-09 11:09:44 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -21,22 +21,58 @@ function Square(props) {
     }
   
     render() {
-      return (
-        <div>
+      
+
+      return (      
+      
+
+        <div>  
+        
+        <h1>Guess Who</h1>
+
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(6)}
             {this.renderSquare(7)}
+        
+          </div>
+          <div className="board-row">
             {this.renderSquare(8)}
+            {this.renderSquare(9)}
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+            {this.renderSquare(14)}
+            {this.renderSquare(15)}
+ 
+          </div>
+          <div className="board-row">
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+            {this.renderSquare(20)}
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+     
+          </div>
+          <div className="board-row">
+            {this.renderSquare(24)}
+            {this.renderSquare(25)}
+            {this.renderSquare(26)}
+            {this.renderSquare(27)}
+            {this.renderSquare(28)}
+            {this.renderSquare(29)}
+            {this.renderSquare(30)}
+            {this.renderSquare(31)}
+         
           </div>
         </div>
       );
